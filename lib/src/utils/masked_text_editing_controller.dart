d6d175aa647e2c4e80df0d64ef32077e6ea31520
Gonzalo Juarez <gonojuarez17@gmail.com>
2025-07-17 18:46:08 -0300

    Refactor: Improve phone number formatting and introduce MaskedTextEditingController
    This commit refactors the phone number input handling in `PhoneSummaryView` and introduces a new `MaskedTextEditingController`.
    
    **Key changes:**
    
    *   **`PhoneSummaryView.dart`:**
        *   Replaced `TextEditingController` with the new `MaskedTextEditingController` for `_phoneEditingController`.
        *   The `country` variable is now initialized in `loadLanguage` and used to set the mask for `_phoneEditingController`.
        *   The `getInputFormatter` method was removed as its functionality is now handled by `MaskedTextEditingController`.
        *   The `phoneTextField` method was simplified by removing the `isValid` parameter and the direct application of `MaskedTextInputFormatter`.
        *   Phone number formatting logic was updated to correctly display the number without the dial code using the mask.
    *   **`lib/src/utils/masked_text_editing_controller.dart`:**
        *   Created a new `MaskedTextEditingController` class that extends `TextEditingController`.
        *   This controller automatically formats the input text based on a provided mask.
        *   It includes a `setMask` method to define the formatting pattern.
        *   The `formatWithMask` method handles the actual formatting logic, replacing mask characters (`#`) with input digits.

---
+++ lib/src/utils/masked_text_editing_controller.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+
+class MaskedTextEditingController extends TextEditingController{
+  String _separator ="#";
+
+  String _mask ='';
+
+  String _aux='';
+
+  void setMask(String mask){
+    this._mask =mask;
+  }
+
+  @override
+  set text(String newText) {
+    if(_mask.isEmpty){
+      super.text = newText;
+      selection = TextSelection.collapsed(offset: super.text.length);
+      return;
+    }
+      String aux = newText;
+       aux = newText;
+       final formated = formatWithMask(aux);
+       super.text = formated;
+    selection = TextSelection.collapsed(offset: super.text.length);
+  }
+
+
+
+  String formatWithMask(String input) {
+    if (_mask.isEmpty) return input;
+
+    final digits = input.replaceAll(RegExp(r'[^\d]'), '');
+    final buffer = StringBuffer();
+    String aux =_mask;
+    for(int i= 0; i< digits.length;i++){
+      aux = aux.replaceFirst(_separator,digits[i]);
+    }
+    List<String> items = aux.split(_separator);
+    buffer.write(items[0]);
+    return buffer.toString();
+  }
+
+  @override
+  // TODO: implement value
+  TextEditingValue get value => super.value;
+  @override
+  set value(TextEditingValue newValue) {
+    // TODO: implement value
+    super.value = newValue;
+  }
+}
